a65022f9f9709bd9a34fc5f54161cab5d81dac4c
Imran Parthib <imranparthib13@gmail.com>
2025-04-04 22:19:12 +0600

    ✨ Add simple Hello World program in Go

---
+++ GolangBackend Development/HelleGo/helloGo.go
@@ -0,0 +1,14 @@
+/*
+This is a simple Hello World program in Go.
+It prints "Hello, World!" to the console.
+Author: Imran Parthib
+Date: 2025-04-04
+Time: 10:00 AM
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
