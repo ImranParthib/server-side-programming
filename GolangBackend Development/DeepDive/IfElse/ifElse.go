74b5d80bd5e876e09d76bb9d2d90ba9e121e677d
Imran Parthib <imranparthib13@gmail.com>
2025-04-05 20:52:44 +0600

    ✨ Add simple if-else statement demonstration program in Go

---
+++ GolangBackend Development/DeepDive/IfElse/ifElse.go
@@ -0,0 +1,19 @@
+/*
+Description: This is a simple program that demonstrates the use of if-else statements in Go.
+Author: Imran Parthib
+Time: 05 April 2025, 8:44 PM
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	myAge := 18
+
+	if myAge >= 18 {
+		fmt.Println("I am eligible to give vote")
+	} else {
+		fmt.Println("I am not eligible to give vote")
+	}
+}
